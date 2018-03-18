fa008939aa28acb5d64cc5812fc55a60c3a719bf
Sean Nikravesh <seannik@bluewin.ch>
2018-03-19 00:08:11 +0900

    place display formatted to flex box

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import React, { Component } from 'react';
 import './App.css';
 import Place from './components/place';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      places: []
+    };
+  }
   render() {
     const place = {
       "name": "Hub",
       "price": "700",
       "imageUrl": "https://www.pub-hub.com/images/shop/main/5576.jpg"
     };
+    const places = [place, place, place];
     return (
-      <div>
-        <Place place={place} />
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          </div>
+          <div className="places">
+            {places.map((place) => {
+              return <Place place={place} />
+            })}
+          </div>
+        </div>
+        <div className="map">
+        </div>
       </div>
     );
   }
